6fdd337e109498e700cd7dae493be2ef2def6b94
dmr-cdda <129845565+dmr-cdda@users.noreply.github.com>
2023-04-17 22:35:21 +0600

    updated carousel container types

--- Types/carouselContainerTypes.js
+++ Types/carouselContainerTypes.js
@@ -1,1 +1,17 @@
-export type CarouselContainerTypes = {}
+export type CarouselContainerTypes :  {
+    className: string;
+    desktopCardSize: number;
+    mobileCardSize: number;
+    tabletCardSize: number;
+    style: {};
+    styleMob: {};
+    styleTab: {};
+} = {
+  className : "",
+  desktopCardSize : 100/4,
+  mobileCardSize : 100/1,
+  tabletCardSize : 100/2,
+  style : {display : "flex", gap: "2.5rem", margin : "2.5rem 0px", transition : "all 0.8s ease-in-out", padding : "0px 1.5rem"},
+  styleMob : {display : "flex", gap: "2.5rem", margin : "2.5rem 0px", transition : "all 0.5s ease-in-out", padding : "0px 1.5rem"}, 
+  styleTab : {display : "flex", gap: "2.5rem", margin : "2.5rem 0px", transition : "all 0.5s ease-in-out", padding : "0px 1.5rem"},
+};
